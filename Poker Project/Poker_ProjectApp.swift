6f2e6033b6b1257366104c43a2c0b76df0189c98
x2lev <lev.kryvenko@gmail.com>
2024-02-16 12:33:54 -0700

    Initial Commit

---
+++ Poker Project/Poker_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Poker_ProjectApp.swift
+//  Poker Project
+//
+//  Created by Kryvenko, Lev on 2/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Poker_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
